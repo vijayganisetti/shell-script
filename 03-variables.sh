5e0f4aa94ce1864212a98f801bdde48f13101b95
vijayganisetti <ganisettisai@gmail.com>
2024-06-22 21:30:06 +0530

    first

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,5 +6,5 @@ person1=vijay
 person2=sai
 
 echo "$person1 : hii $person2 how are you ?"
-echo "$person1 : hi $person2 i am good "
+echo "$person2 : hi $person1 i am good "
 echo "$person1 : where are you living know "
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# argumensts passing from out side 
+person1=$1
+person2=$2
+
+echo "$person1 : hii $person2 how are you ?"
+echo "$person2 : hi $person1 i am good "
+echo "$person1 : where are you living know "
