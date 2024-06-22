7a401938db51254ce4b480c127fa74a38add79ff
vijayganisetti <ganisettisai@gmail.com>
2024-06-22 21:26:44 +0530

    first

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,4 @@
-!#/bin/bash 
+#!/bin/bash 
 ##shebang 
 
 echo "hello-world"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# declaring variables 
+
+person1=vijay
+person2=sai
+
+echo "$person1 : hii $person2 how are you ?"
+echo "$person1 : hi $person2 i am good "
+echo "$person1 : where are you living know "
