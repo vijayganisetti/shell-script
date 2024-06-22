42aa847fdfaba14d06c7cda7c4fbf76a85f8c49e
vijayganisetti <ganisettisai@gmail.com>
2024-06-22 21:56:32 +0530

    first

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash 
 
 echo "to print all variables : $@"
+echo " to know no of variables : $#"
+echo " to know script name : $0"
+echo " to know current working directory : $PWD"
+echo " to know the home directoty : $HOME"
+echo " to knoe the user : $USER"
+echo " to know the server host name : $HOSTNAME"
+echo " to know process id of current shell script : $$"
+echo " to know process id of last background command : $!"
