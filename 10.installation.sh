79b8ad449a7302514d854140b06c1a23d0e42195
vijayganisetti <ganisettisai@gmail.com>
2024-06-23 11:25:03 +0530

    first

--- 10.installation.sh
+++ 10.installation.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
 echo "plz run with super user"
-exit 1
+  exit 1
 else
-echo "running with super user"
+  echo "running with super user"
 fi
 
 dnf install mysql -y
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then 
 echo "failed to install mysql"
 exit 1
@@ -19,7 +19,7 @@ echo "failed to install mysql"
 fi 
 dnf install git -y
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then 
 echo "failed to install mysql"
 exit 1
