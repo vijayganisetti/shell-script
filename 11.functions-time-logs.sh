5da5fa9871d02b292c6a0b70f84e5b2b800dd09a
vijayganisetti <ganisettisai@gmail.com>
2024-06-23 16:55:35 +0530

    first

---
+++ 11.functions-time-logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+## this script is combination of functions , conditions , timestamp , and log files and script name and colours 
+
+USERID=$(id -u)
+timestamp=$(date +%f-%h-%m-%S-%Y)
+scriptname=$(echo $0 | cut -d "." -f 1)
+logsfile=$timestamp-$scriptname.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+validate () {
+    if [ $1-ne 0]
+    then 
+    echo -e"$2 $R is failed"
+    exit 1
+    else
+    echo -e"$2 $G is success"
+    fi
+}
+
+if [ $USERID -ne 0]
+then 
+echo "try with the superuser"
+exit 1
+else 
+echo "running with superuser"
+fi 
+dnf install chrony -y
+validate $? "installation of chrony"
+
+dnf install gcc -y
+validate $? "installation of gcc"
+
