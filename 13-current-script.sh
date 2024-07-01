e6e6a85914eab42d85abd2b351f6d6d263f8eaeb
vijayganisetti <ganisettisai@gmail.com>
2024-07-01 13:59:17 +0530

    first

--- 13-current-script.sh
+++ 13-current-script.sh
@@ -5,6 +5,9 @@ COURSE="devops from current script"
 echo "defore calling others script : $COURSE"
 echo "the process id of current shell script : $$"
 
-./14-other-script.sh
+## calling others script
+ #./14-others-script.sh ## 1st way to call others script 
+
+source./14-other-script.sh ## 2nd way to call others script
 
 echo "after calling others script : $COURSE"
