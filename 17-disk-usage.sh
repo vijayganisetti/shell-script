5cdc1ffd8a685403c177ae229ba5232730a23822
vijayganisetti <ganisettisai@gmail.com>
2024-07-03 16:17:06 +0530

    first

---
+++ 17-disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -ht | grep xfs)
+DISK_STORM=75
+
+while IFS = read -r line
+do
+ USAGE=$($DISK_USAGE | awk -F " " '{print $6F}' | cut -d "%" -f1)
+FILE_NAME=$($DISK_USAGE | awk -F " " '{print $NF}')
+
+  if [ $USAGE -ge $DISK_STORM ]
+  then 
+  echo "$FILE_NAME is more than $DISK_STORM% used current usage : $USAGE"
+  
+done <<<$DISK_USAGE
+
