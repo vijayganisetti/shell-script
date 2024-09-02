88c3220749ed90d0ee4668e073037f2b9cb50750
vijayganisetti <ganisettisai@gmail.com>
2024-09-02 20:51:55 +0530

    shell

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,10 +2,10 @@
 
 num=$1
 
-if [$num -gt 10]
+if [ $num -gt 10 ]
 then
     echo "number is greater than 10"
-elif [$num -lt 10]
+elif [ $num -lt 10 ]
 then
     echo "number is less than 10"
 else
