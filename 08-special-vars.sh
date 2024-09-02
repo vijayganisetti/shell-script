2c4cee0afdde517265a78b88cfd070befae35b6a
vijayganisetti <ganisettisai@gmail.com>
2024-09-02 20:43:52 +0530

    shell

---
+++ 08-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "to print all varaibles : $@ "
+echo " to know no of vars : $# "
+echo " to know script -0name : $0"
+echo " to know working directoru : $PWD"
+echo " to know home directory : $HOME "
+echo " to know which user running the script : $USER"
+echo " to know Host name : $HOSTNAME"
+echo " to know pid num : $$ "
+echo " to know background pid : $!"
+echo : to know exit status : $?"
