2a933984427e048d4b4088d5bb33d3ecd46371d3
vijayganisetti <ganisettisai@gmail.com>
2024-09-02 20:28:52 +0530

    shell

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#confidential like username and password write in this way 
+
+echo "Enter username" 
+read -s USERNAME
+echo "Enter password "
+read -s PASSWORD
+
+echo " your username is $USERNAME and password is $PASSWORD
