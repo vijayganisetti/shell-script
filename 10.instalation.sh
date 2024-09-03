aaa7f56a35f73cdb2e5dc48b4e6fafe2fbb80034
vijayganisetti <ganisettisai@gmail.com>
2024-09-03 16:50:16 +0530

    shell

---
+++ 10.instalation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+echo "you are not a supersuper try with super user"
+exit 1
+else
+echo "you are a super user"
+fi
+dnf install mysql -u 
+
+if [ $? -ne 0 ]
+then 
+echo "installing sql is failed"
+exit 1
+else 
+echo " installing is succes "
+fi 
+
+dnf install git -u 
+
+if [ $? -ne 0 ]
+then 
+echo " installing git is failed "
+else 
+echo " installing git succes "
+fi 
+
